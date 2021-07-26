8290a94688ebed657ac3cfbfa9e36aed15d125cf
gabrielcarioca <gabrielcarioca23@gmail.com>
2021-07-26 00:47:40 -0300

    Added parsing and validation for api requests when in demo event page

--- pages/demo_event/demo_event_home_page.py
+++ pages/demo_event/demo_event_home_page.py
@@ -2,6 +2,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 
+import json
+
 from Utils.api_request_not_sent import APIRequestNotSent
 
 class DemoEventHomePage:
@@ -18,30 +20,36 @@ class DemoEventHomePage:
     def wait_until_api_requests_are_sent(self):
 
         # Waiting for the requests to be sent
-        sent_requests = self.wait.until(
+        requests_sent = self.wait.until(
             WaitDemoEventAPIRequests()
         )
 
         # Seeing if the requests weren't found
-        if sent_requests['event_show'] == '':
+        if requests_sent['event_show'] == '':
             raise APIRequestNotSent('event_show API request not sent in demo event page')
-        elif sent_requests['event_information_show'] == '':
+        elif requests_sent['event_information_show'] == '':
             raise APIRequestNotSent('event_interstitial_information_show API request not sent in demo event page')
+
+        return requests_sent
 
 class WaitDemoEventAPIRequests:
     def __init__(self):
-        self.sent_requests = {'event_show': '', 'event_information_show': ''}
+        self.requests_sent = {'event_show': '', 'event_information_show': ''}
 
     def __call__(self, driver):
         for log in driver.get_log('performance'):
             log_message = log['message']
             if 'event_show' in log_message:
-                self.sent_requests['event_show'] = log_message
+                self.requests_sent['event_show'] = json.loads(log_message)['message']['params']
+                self.requests_sent['event_show']['request']['postData'] = json.loads(
+                    self.requests_sent['event_show']['request']['postData'])
             elif 'event_interstitial_information_show' in log_message:
-                self.sent_requests['event_information_show'] = log_message
+                self.requests_sent['event_information_show'] = json.loads(log_message)['message']['params']
+                self.requests_sent['event_information_show']['request']['postData'] = json.loads(
+                    self.requests_sent['event_information_show']['request']['postData'])
 
-            if self.sent_requests['event_show'] != '' and self.sent_requests['event_information_show'] != '':
-                return self.sent_requests
+            if self.requests_sent['event_show'] != '' and self.requests_sent['event_information_show'] != '':
+                return self.requests_sent
 
-        if self.sent_requests['event_information_show'] == '' or self.sent_requests['event_show'] == '':
+        if self.requests_sent['event_information_show'] == '' or self.requests_sent['event_show'] == '':
             return False
--- tests/demo_event/test_demo_event.py
+++ tests/demo_event/test_demo_event.py
@@ -1,12 +1,14 @@
 import time
 
+from datetime import datetime, timedelta
 from login.login_page import LoginPage
 from demo_event.demo_event_home_page import DemoEventHomePage
 from demo_event.demo_event_attendees_page import DemoEventAttendeesPage
 
-import sys
-
 def test_search_attendees(browser, user_data):
+    demo_event_id = 'RXZlbnRfMTg5Njc2'
+    demo_event_information_id = 'QWR2ZXJ0aXNlbWVudF8yODYz'
+
     # Going to app page
     browser.get('https://app.swapcard.com')
 
@@ -27,7 +29,59 @@ def test_search_attendees(browser, user_data):
     # Going to demo event page
     demo_event_home_page.demo_event_card().click()
     # Validating API requests
-    demo_event_home_page.wait_until_api_requests_are_sent()
+    api_requests = demo_event_home_page.wait_until_api_requests_are_sent()
+    now = datetime.now()
+    request_time_delta = timedelta(minutes=2)
+
+    errors = []
+    # Checking URL for request
+    event_show = api_requests['event_show']['request']
+    event_information = api_requests['event_information_show']['request']
+    for key in api_requests:
+        request_url = api_requests[key]['request']['url']
+        if not request_url == 'https://t.swapcard.com/':
+            errors.append(f"URL for {key} request was not https://t.swapcard.com/, it was {request_url}")
+
+    # Checking payload for event_show
+    event_id = event_show['postData']['event_id']
+    if not event_id == demo_event_id:
+        errors.append(
+            f"Event Id request for event_show in demo event was {event_id} instead of {demo_event_id}")
+
+    event_type = event_show['postData']['type']
+    if not event_type == 'event_show':
+        errors.append(
+            f"Request type for event_show in demo event was {event_type} instead of event_show")
+
+    event_data = datetime.strptime(event_show['postData']['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    if not datetime.now() - event_data < request_time_delta:
+        errors.append(
+            f"Request date for event_show was {event_data}")
+
+    # Checking payload for event_information
+    event_id = event_information['postData']['event_id']
+    if not event_id == demo_event_id:
+        errors.append(
+            f"Event Id request for event_interstitial_information_show in demo event was {event_id} instead of {demo_event_id}")
+
+    information_id = event_information['postData']['information_id']
+    if not information_id == demo_event_information_id:
+        errors.append(
+            f"Demo event information Id for event_interstitial_information_show in demo event was {information_id} instead of {demo_event_information_id}")
+
+    event_type = event_information['postData']['type']
+    if not event_type == 'event_interstitial_information_show':
+        errors.append(
+            f"Request type for event_interstitial_information_show in demo event was {event_type} instead of event_interstitial_information_show")
+
+    event_data = datetime.strptime(event_information['postData']['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    if not datetime.now() - event_data < request_time_delta:
+        errors.append(
+            f"Request date for event_show was {event_data}")
+
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+    assert browser.current_url == "https://app.swapcard.com/event/your-demo-event-demo-swapcard-62"
+
     # Going to the attendees page
     demo_event_attendees_page.atendees_tab_selector().click()
 
