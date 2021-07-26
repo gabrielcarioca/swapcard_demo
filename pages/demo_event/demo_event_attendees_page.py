03227fa583e8cca117a159d80046afbc9a0ab274
gabrielcarioca <gabrielcarioca23@gmail.com>
2021-07-26 17:16:08 -0300

    Added validation for API request when searching attendees

--- pages/demo_event/demo_event_attendees_page.py
+++ pages/demo_event/demo_event_attendees_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 
-import re
+import re, json
 
 
 class DemoEventAttendeesPage:
@@ -11,9 +11,11 @@ class DemoEventAttendeesPage:
     REFINE_THE_LIST_SEARCH_MESSAGE = (By.XPATH, "//div[./p[text()='Refine the list (min. 2 characters)']]")
     NUMBER_OF_ATTENDEES_IN_SEARCH_RESULT = (By.XPATH, "//div[./p[contains(text(), 'result')]]")
 
+    TIMEOUT = 15
+
     def __init__(self, browser):
         self.browser = browser
-        self.wait = WebDriverWait(self.browser, 10)
+        self.wait = WebDriverWait(self.browser, self.TIMEOUT)
 
     def attendees_tab_selector(self):
         """ Attendees link in the tab selector on top of the event page """
@@ -33,3 +35,40 @@ class DemoEventAttendeesPage:
         search_result = self.wait.until(expected_conditions.visibility_of_element_located(self.NUMBER_OF_ATTENDEES_IN_SEARCH_RESULT))
         assert bool(re.match(r"\d+\sresults", search_result.text))
 
+    def wait_until_api_request_when_filling_search_field(self):
+        """ Check if people_view_search API request is found in browser performance log or if it is sent and found in the next 15 seconds """
+        # Waiting for the request to be sent
+        request_sent = self.wait.until(
+            WaitPeopleSearchAPIRequests()
+        )
+
+        # Seeing if the requests weren't found
+        if request_sent['people_view_search'] == '':
+            raise APIRequestNotSent(
+                f'people_view_search API request was not sent in attendees page for {self.TIMEOUT} seconds after filling the search field')
+
+        return request_sent
+
+class WaitPeopleSearchAPIRequests:
+    """ Class with a __call__ method used to validate if people_view_search is found in the performance log for driver
+
+        Attributes:
+            driver  The driver or browser to check the performance log for the expected API request
+    """
+
+    def __init__(self):
+        self.request_sent = {'people_view_search': ''}
+
+    def __call__(self, driver):
+        for log in driver.get_log('performance'):
+            log_message = log['message']
+            if 'people_view_search' in log_message:
+                self.request_sent['people_view_search'] = json.loads(log_message)['message']['params']
+                self.request_sent['people_view_search']['request']['postData'] = json.loads(
+                    self.request_sent['people_view_search']['request']['postData'].split("\n")[1])
+
+                # Returning the requests if they are found
+                return self.request_sent
+
+        # Otherwise returning false if the expected requests was not found in the attendees page after filling the search field
+        return False
--- tests/demo_event/test_demo_event.py
+++ tests/demo_event/test_demo_event.py
@@ -7,10 +7,13 @@ from demo_event.demo_event_attendees_page import DemoEventAttendeesPage
 
 
 class DemoEventTests:
+    demo_event_id = 'RXZlbnRfMTg5Njc2'
+    demo_event_information_id = 'QWR2ZXJ0aXNlbWVudF8yODYz'
+    demo_event_view_id = 'RXZlbnRWaWV3Xzc0MjU3'
 
     def test_demo_event_page(self, browser, user_data):
-        demo_event_id = 'RXZlbnRfMTg5Njc2'
-        demo_event_information_id = 'QWR2ZXJ0aXNlbWVudF8yODYz'
+
+
 
         # Going to Swapcard app page
         browser.get('https://app.swapcard.com')
@@ -37,51 +40,53 @@ class DemoEventTests:
         errors = []
         # Checking requests URL
         event_show = api_requests['event_show']['request']
+        event_show_post_data = event_show['postData']
         event_information = api_requests['event_information_show']['request']
+        event_information_post_data = event_information['postData']
         for key in api_requests:
             request_url = api_requests[key]['request']['url']
             if not request_url == 'https://t.swapcard.com/':
                 errors.append(f"URL for {key} request was not https://t.swapcard.com/, it was {request_url}")
 
         # Checking event_show payload
-        event_id = event_show['postData']['event_id']
-        if not event_id == demo_event_id:
+        event_id = event_show_post_data['event_id']
+        if not event_id == self.demo_event_id:
             errors.append(
-                f"Event Id request for event_show in demo event was {event_id} instead of {demo_event_id}")
+                f"Event Id payload for event_show in demo event was {event_id} instead of {self.demo_event_id}")
 
-        event_type = event_show['postData']['type']
-        if not event_type == 'event_show':
+        request_type = event_show_post_data['type']
+        if not request_type == 'event_show':
             errors.append(
-                f"Request type for event_show in demo event was {event_type} instead of event_show")
+                f"Request type for event_show in demo event was {request_type} instead of event_show")
 
-        event_data = datetime.strptime(event_show['postData']['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        if not datetime.now() - event_data < request_time_delta:
+        request_date = datetime.strptime(event_show_post_data['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        if not datetime.now() - request_date < request_time_delta:
             errors.append(
-                f"Request date for event_show was {event_data}")
+                f"Request date for event_show was {request_date}")
 
         # Checking event_interstitial_information_show payload
-        event_id = event_information['postData']['event_id']
-        if not event_id == demo_event_id:
+        event_id = event_information_post_data['event_id']
+        if not event_id == self.demo_event_id:
             errors.append(
-                f"Event Id request for event_interstitial_information_show in demo event was {event_id} instead of {demo_event_id}")
+                f"Event Id payload for event_interstitial_information_show in demo event was {event_id} instead of {self.demo_event_id}")
 
-        information_id = event_information['postData']['information_id']
-        if not information_id == demo_event_information_id:
+        information_id = event_information_post_data['information_id']
+        if not information_id == self.demo_event_information_id:
             errors.append(
-                f"Demo event information Id for event_interstitial_information_show in demo event was {information_id} instead of {demo_event_information_id}")
+                f"Demo event information Id for event_interstitial_information_show in demo event was {information_id} instead of {self.demo_event_information_id}")
 
-        event_type = event_information['postData']['type']
-        if not event_type == 'event_interstitial_information_show':
+        request_type = event_information_post_data['type']
+        if not request_type == 'event_interstitial_information_show':
             errors.append(
-                f"Request type for event_interstitial_information_show in demo event was {event_type} instead of event_interstitial_information_show")
+                f"Request type for event_interstitial_information_show in demo event was {request_type} instead of event_interstitial_information_show")
 
-        event_data = datetime.strptime(event_information['postData']['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        if not datetime.now() - event_data < request_time_delta:
+        request_date = datetime.strptime(event_information_post_data['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        if not datetime.now() - request_date < request_time_delta:
             errors.append(
-                f"Request date for event_show was {event_data}")
+                f"Request date for event_show was {request_date}")
 
         # Asserting if all API requests were send as expected with correct payload and URL
-        assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+        assert not errors, "Errors occurred:\n{}".format("\n".join(errors))
         assert browser.current_url == "https://app.swapcard.com/event/your-demo-event-demo-swapcard-62"
 
     def test_attendees_search_in_demo_event(self, browser):
@@ -92,7 +97,50 @@ class DemoEventTests:
         demo_event_attendees_page.attendees_tab_selector().click()
 
         # Filling search field
-        demo_event_attendees_page.fill_attendees_search_field('LLC')
+        query = 'LLC'
+        demo_event_attendees_page.fill_attendees_search_field(query)
+
+        # Validating API requests
+        api_requests = demo_event_attendees_page.wait_until_api_request_when_filling_search_field()
+        request_time_delta = timedelta(minutes=2)
+
+        errors = []
+        # Checking request URL
+        people_search = api_requests['people_view_search']['request']
+        people_search_post_data = people_search['postData']
+
+        if not people_search['url'] == 'https://t.swapcard.com/':
+            errors.append(f"URL for people_view_search request was not https://t.swapcard.com/, it was {people_search['url']}")
+
+        # Checking people_view_search payload
+        event_id = people_search_post_data['event_id']
+        if not event_id == self.demo_event_id:
+            errors.append(
+                f"Event Id payload for people_view_search in demo event attendees search was {event_id} instead of {self.demo_event_id}")
+
+        event_view_id = people_search_post_data['event_view_id']
+        if not event_view_id == self.demo_event_view_id:
+            errors.append(
+                f"Event view Id payload for people_view_search in demo event attendees search was {event_view_id} instead of {self.demo_event_information_id}")
+
+        search_query = people_search_post_data['query']
+        if not search_query == query:
+            errors.append(
+                f"Query for attendees search in demo event was expected to be {query} but was instead {search_query}")
+
+        search_requests_type = people_search_post_data['type']
+        if not search_requests_type == 'people_view_search':
+            errors.append(
+                f"Request type for people_view_search in attendees search for demo event was {search_requests_type} instead of people_view_search")
+
+        search_date = datetime.strptime(people_search_post_data['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        if not datetime.now() - search_date < request_time_delta:
+            errors.append(
+                f" Request date for people_view_search was {search_date}")
+
+        # Asserting if all API requests were send as expected with correct payload and URL
+        assert not errors, "Errors occurred:\n{}".format("\n".join(errors))
+        assert browser.current_url == f"https://app.swapcard.com/event/your-demo-event-demo-swapcard-62/people/RXZlbnRWaWV3Xzc0MjU3?search={query}"
 
         time.sleep(5)
         assert True
